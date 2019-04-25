bcbe38dba256d2d9cbf40b80e4ef2f47bdc8d66a
hansleolml <hansleolml@gmail.com>
2019-04-25 16:11:47 -0500

    Revert "asdas"
    This reverts commit 99c84c9323dd1572e86dd3c29721da6de25ea8e0.

--- app/User.php
+++ app/User.php
@@ -9,9 +9,6 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    public function roles(){
-        return $this->belongsToMany('App\Role')
-    }
     /**
      * The attributes that are mass assignable.
      *
