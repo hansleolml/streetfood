62d10ab4d3940a65870899132f09bcd0c208f731
hansleolml <hansleolml@gmail.com>
2019-05-02 14:02:25 -0500

    perfil controlador y blade

--- resources/views/miperfil.blade.php
+++ resources/views/miperfil.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.marco')
+@section('externolink')
+<link href="tokenfield/dist/css/bootstrap-tokenfield.min.css" rel="stylesheet">
+	<link href="jquery-ui/jquery-ui.min.css" rel="stylesheet">
 
+	<!-- <link href="estilos/carousel.css" rel="stylesheet"> -->
+    <link href="{{asset('css/product.css')}}" rel="stylesheet">
+    <link href="{{asset('css/carousel.css')}}" rel="stylesheet">
+@endsection
+@section('externoscript')
+<script	src="{{asset('jquery-ui/jquery-ui.min.js')}}"> </script>
+	<script src="{{asset('tokenfield/dist/bootstrap-tokenfield.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/tags.js')}}"></script>
 
+@endsection
+@section('content')
 <div class="col-md-8">
-	<div class="row">
-		<div class="col-md-8">
 			<div class="panel panel-default">
 			  	<div class="panel-heading">
 			    	<h3 class="panel-title">Perfil</h3>
@@ -11,56 +22,56 @@
 			  	<!-- perfil -->
 			  	<div class="panel-body">
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Nombres:</strong>
-				    		<span class="pull-right">Lawrence Vennadite</span>
+				    		<span class="pull-right">{{auth()->user()->name}}</span>
 				    	</div>
 				   
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Apellidos:</strong>
-				    		<span class="pull-right">Lawrence Vennadite</span>
+				    		<span class="pull-right">{{auth()->user()->apellidos}}</span>
 				    	</div>
 				    
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Sexo:</strong>
 				    		<span class="pull-right">Hombre</span>
 				    	</div>
 				    	
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Fecha de Nacimiento:</strong>
 				    		<span class="pull-right">15/08/1997</span>
 				    	</div>
 				    	
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Estudios:</strong>
 				    		<span class="pull-right">Medicina en Universidad Mayor de San Marcos</span>
 				    	</div>
 				    	
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>De:</strong>
 				    		<span class="pull-right">Perú, Lima</span>
 				    	</div>
 				    	
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Email:</strong>
 				    		<span class="pull-right">Jennifer_12@gmail.com</span>
 				    	</div>
 				    	
 				    </div>
 				    <div class="row">
-				    	<div class="col-sm-10">
+				    	<div class="col-sm-12">
 				    		<strong>Relación:</strong>
 				    		<span class="pull-right">Soltera</span>
 				    	</div>
@@ -68,7 +79,7 @@
 				    </div>
 				    <div class="row">
 				    	<div class="col-sm-12">
-				    		<button class="btn btn-default pull-right" right type="submit">
+				    		<button class="btn btn-primary pull-right" right type="submit">
 				    		Actualizar
 				    		</button>
 				    	</div>
@@ -76,55 +87,4 @@
 			  	</div>
 			</div>
 		</div>
-		<!-- card perfil -->
-		<div class="col-md-4">
-			<div class="thumbnail">
-		      	<img src="imagenes/sad-status-1691593_1280.jpg" class="img-responsive img-rounded" alt="Foto perfil">
-		      	<div class="caption">
-		        	<h3>Jeniffer Lawrence</h3>
-		        	<p>
-		        		Soy una persona bla Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, aut!
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda modi, fugiat laboriosam aspernatur sed reprehenderit eaque, maiores repellendus molestias deserunt.
-						<button class="btn btn-default btn-sm" type="submit">
-			    			<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-				    	</button>
-		        	</p>
-		        	<p>
-		        		<a href="#" class="btn btn-primary" role="button">Actualizar foto</a> 
-		        		<a href="#" class="btn btn-default pull-right" role="button">Borrar foto</a>
-		        	</p>
-		      	</div>
-		    </div>
-		</div>
-	</div>
-	<!-- 	<div class="row">
-			<div class="col-md-12">
-				<div class="panel panel-default">
-				  	<div class="panel-heading">
-				    	<h3 class="panel-title">Interacciones</h3>
-				  	</div>
-				  	<div class="panel-body">
-					    <div class="row">
-					    	<div class="col-sm-12">
-					    		<h3>Reviews Hechos<small>(relevancia/fecha)</small></h3>
-					    		<p>1.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014 <strong>SOBRESALIENTE</strong></p>
-					    		<p>2.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014</p>
-					    		<p>3.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014 <strong>SOBRESALIENTE</strong></p>
-					    		<p>4.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014</p>
-					    		<p>5.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014</p>
-					    		<p>6.-<a href="#">¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ab.?</a> #interacciones = 150 fecha 14/08/2014</p>
-					    		<hr style="width: 100%; color: black; height: 1px; background-color:black;" />
-					    		<h3>Reviews recibidos<small>(relevancia/fecha)</small></h3>
-					    		<p>1.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014</p>
-					    		<p>2.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014</p>
-					    		<p>3.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014 <strong>SOBRESALIENTE</strong></p>
-					    		<p>4.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014</p>
-					    		<p>5.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014</p>
-					    		<p>6.-Lorem ipsum dolor sit amet.<a href="#">..ver más</a> en <a href="#">¿Lorem ipsum dolor sit....?</a> Likes=150  Dislikes=12 fecha 14/08/2014 <strong>SOBRESALIENTE</strong></p>
-					    	</div>
-					    </div>
-				  	</div>
-				</div>
-			</div>
-		</div> -->
-</div>
+@endsection
